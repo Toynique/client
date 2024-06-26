87ee28a1a7eb198f998356ef2ee6dcac6c3b1597
manishkrt <manishkirthalya@gmail.com>
2024-06-26 13:59:40 +0530

    update

--- src/url/url.js
+++ src/url/url.js
@@ -1,3 +1,3 @@
 // export const Url = "http://localhost:8080"
 export const ImageUrl = "http://doozie-do.cloud"
-export const Url = "https://boujeetoys.com/" 
+export const Url = "https://boujeetoys.com" 
